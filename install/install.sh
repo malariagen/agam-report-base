56b4eb415d268792ab38b9b41ed6b07f7b94316b
Alistair Miles <alimanfoo@googlemail.com>
2017-09-21 14:21:16 +0100

    solve dependency and install issues with matplotlib/graphviz

--- install/install.sh
+++ install/install.sh
@@ -5,21 +5,22 @@
 
 
 # ensure script errors if any command fails
-set -eo pipefail
+set -e
 
 
-# determine name of current directory, use as conda environment name
+# conda setup
+CONDADIR=conda
 CONDANAME=${PWD##*/}
 
-
 # descend into dependencies directory
-mkdir -pv dependencies
-cd dependencies
+DEPSDIR=deps
+mkdir -pv $DEPSDIR
+cd $DEPSDIR
 
 
 # put dependencies on the path
 export PATH=./texlive/bin/x86_64-linux:$PATH
-export PATH=./miniconda/bin:$PATH
+export PATH=./${CONDADIR}/bin:$PATH
 
 
 # use a snapshot mirror to get reproducible install
@@ -31,18 +32,19 @@ if [ ! -f miniconda.installed ]; then
     echo "[install] installing miniconda"
 
     # clean up any previous
-    rm -rf miniconda
+    rm -rf $CONDADIR
 
     # download miniconda
     wget --no-clobber https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
     # install miniconda
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda
-    conda upgrade --yes conda
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDADIR
 
     # set conda channels
+    conda config --add channels defaults
     conda config --add channels bioconda
     conda config --add channels conda-forge
+    conda upgrade --yes conda
 
     # create default scientific Python environment
     conda create --yes --name=$CONDANAME python=3.6
@@ -87,6 +89,7 @@ fi
 echo "[install] installing Python packages"
 source activate $CONDANAME
 # ensure conda channels
+conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 # conda install
